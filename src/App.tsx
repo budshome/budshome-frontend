c9eccc09dedbd36cd12add4a8ddc4633cc7ff07a
zzy <9809920@qq.com>
2019-02-05 14:19:30 +0800

    -

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import React, { Component } from 'react';
 import logo from './images/logo.svg';
 import './App.css';
 
+import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            <img src={logo} className="App-logo" alt="logo" />
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <PrimaryButton>BudsHome-cms</PrimaryButton>
         </header>
       </div>
     );
