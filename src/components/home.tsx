7b8ceff321a4fb6f461d837e6695dbf6c504ff31
zzy <9809920@qq.com>
2019-02-06 13:25:24 +0800

    -

---
+++ src/components/home.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import NaviBar from './navigation';
+
+class App extends Component {
+  render() {
+    return (
+      <NaviBar />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navigation.tsx
@@ -0,0 +1,112 @@
+import * as React from 'react';
+
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+
+class NaviBar extends React.Component<{}, {}> {
+  public render(): JSX.Element {
+    return (
+      <div>
+        <CommandBar
+          items={this.getItems()}
+          overflowItems={this.getOverlflowItems()}
+          overflowButtonProps={{ ariaLabel: 'More commands' }}
+          farItems={this.getFarItems()}
+          ariaLabel={'Use left and right arrow keys to navigate between commands'}
+        />
+      </div>
+    );
+  }
+
+  // Data for CommandBar
+  private getItems = () => {
+    return [
+      {
+        key: 'home',
+        name: '首页',
+        iconProps: {
+          iconName: 'Upload'
+        },
+        href: 'http://budshome.com/',
+        ['data-automation-id']: 'uploadButton'
+      },
+      {
+        key: 'share',
+        name: 'Share',
+        iconProps: {
+          iconName: 'Share'
+        },
+        onClick: () => console.log('Share')
+      },
+      {
+        key: 'download',
+        name: 'Download',
+        iconProps: {
+          iconName: 'Download'
+        },
+        onClick: () => console.log('Download')
+      }
+    ];
+  };
+
+  private getOverlflowItems = () => {
+    return [
+      {
+        key: 'move',
+        name: 'Move to...',
+        onClick: () => console.log('Move to'),
+        iconProps: {
+          iconName: 'MoveToFolder'
+        }
+      },
+      {
+        key: 'copy',
+        name: 'Copy to...',
+        onClick: () => console.log('Copy to'),
+        iconProps: {
+          iconName: 'Copy'
+        }
+      },
+      {
+        key: 'rename',
+        name: 'Rename...',
+        onClick: () => console.log('Rename'),
+        iconProps: {
+          iconName: 'Edit'
+        }
+      }
+    ];
+  };
+
+  private getFarItems = () => {
+    return [
+      {
+        key: 'sort',
+        name: 'Sort',
+        iconProps: {
+          iconName: 'SortLines'
+        },
+        onClick: () => console.log('Sort')
+      },
+      {
+        key: 'tile',
+        name: 'Grid view',
+        iconProps: {
+          iconName: 'Tiles'
+        },
+        iconOnly: true,
+        onClick: () => console.log('Tiles')
+      },
+      {
+        key: 'info',
+        name: 'Info',
+        iconProps: {
+          iconName: 'Info'
+        },
+        iconOnly: true,
+        onClick: () => console.log('Info')
+      }
+    ];
+  };
+}
+
+export default NaviBar;
