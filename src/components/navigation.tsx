a0d721b18132d13496c6a5a1f2a46e8a3d76e171
zzy <9809920@qq.com>
2019-02-06 13:30:01 +0800

    -

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 
 import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+
+
 
 class NaviBar extends React.Component<{}, {}> {
   public render(): JSX.Element {
